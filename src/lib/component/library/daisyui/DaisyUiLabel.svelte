58d6989c7a6faeb90a9099ecfa0128e359680c35
LaYaungHtut <layaunghtut5@gmail.com>
2025-06-04 22:52:48 +0630

    Alert, Collapse, FieldSet, InputField, Label, Loading, Toast components added

---
+++ src/lib/component/library/daisyui/DaisyUiLabel.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let { children, className } = $props<{
+        children: () => void;
+        className?: string;
+    }>();
+</script>
+
+<label class="d-input {className}">
+    {@render children()}
+</label>
